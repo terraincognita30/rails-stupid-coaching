3af745b6012271645426387169dc94be9544ae05
Carmen Chung <jlchung416@gmail.com>
2019-08-05 12:47:20 +0900

    what

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'ask', to: 'questions#ask', as: :ask
+  get 'answer', to: 'questions#answer', as: :answer
+  get '/', to: 'questions#home', as: :home
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
